8c8cb21f4876b5f232f124a0c2489a92ee0cbc44
drswinghead <drswinghead@163.com>
2015-04-19 21:24:14 +0000

    improve vdmnt.sh

--- vdmnt.sh
+++ vdmnt.sh
@@ -26,30 +26,56 @@ function vdfuse_umount()
     sudo umount /mnt/vdfuse
 }
 
+function vdfuse_ismounted()
+{
+    mted=$(mount|grep "vsd1"|grep -v grep)
+    if [ x"$mted" == x"" ] ; then
+        return 0
+    else
+        return 1
+    fi
+}
 
 function vdfuse_remount()
 {
     sudo umount /mnt/vsd1
     ret=$?
     if [ x"$ret" != x"0" ] ; then
-        echo "umount error: $ret";
-        exit;
+        vdfuse_ismounted
+        ret2=$?
+        echo "chk mounted:$ret2";
+
+        if [ x"$ret2" == x"1" ] ; then
+            echo "umount error: $ret";
+            exit;            
+        fi
     fi
 
     sleep 1;
     ntfs-3g -o ro /mnt/vdfuse/Partition1 /mnt/vsd1
+
+    sleep 3;
     # ls -lh -colors /mnt/vsd1/ | head -n 5
     echo -e "________________________________________________________";
     tree -L 2  /mnt/vsd1/
 }
 
+function vdfuse_show()
+{
+    # ls -lh -colors /mnt/vsd1/ | head -n 5
+    echo -e "________________________________________________________";
+    tree -L 2  /mnt/vsd1/
+}
 
+arg0=$1
 if [ x"$1" == x"m" ] ; then
     vdfuse_mount;
 elif [ x"$1" == x"u" ] ; then
     vdfuse_umount;
 elif [ x"$1" == x"r" ] ; then
     vdfuse_remount;
+elif [ x"$1" == x"s" ] ; then
+    vdfuse_show;
 else
     echo "vdmnt.sh <m|u|r>";
 fi
